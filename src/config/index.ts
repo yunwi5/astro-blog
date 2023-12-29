a61340e85fc0094f293d9bfc9cd3eed324d61ed5
yunwi5 <1009jyk@gmail.com>
2023-12-29 23:38:43 +1300

    Fix url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 export function getBaseUrl(): string {
-	return process.env.NODE_ENV === "development" ? "http://localhost:4321" : "https://astro-blog-bdwypesim-yunwi5.vercel.app";
+	return process.env.NODE_ENV === "development" ? "http://localhost:4321" : "https://astro-blog-git-main-yunwi5.vercel.app/";
 }
